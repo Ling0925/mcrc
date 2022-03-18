a322037d6dc331b8c5178fa42ea348b19fd5e8f9
2449858657a@gmail.com <ling0925>
2022-03-18 17:45:56 +0800

    add cli argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 use crate::mcrc_core::launcher::launcher;
-
+use clap::Parser;
 mod mcrc_core;
 
 fn main() {
     println!("Hello, world!");
+    let args = Args::parse();
+    if args.online {
+        println!("soon...");
+    }
     println!("{}", launcher());
+
+    println!("{:?}", args);
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    online: bool,
+
+    #[clap(short, long, default_value = "player")]
+    name: String,
+
+    #[clap(short, long, default_value = "./")]
+    game_directory: String,
 }
